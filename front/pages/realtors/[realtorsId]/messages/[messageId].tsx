77f092adbc65bb22f944b90f75da441e76a1971d
itekhaj <jean-philippe.kha@i-bp.fr>
2022-01-13 16:35:00 +0100

    features(messages-detail): call message detail server side

--- front/pages/realtors/[realtorsId]/messages/[messageId].tsx
+++ front/pages/realtors/[realtorsId]/messages/[messageId].tsx
@@ -1,26 +1,26 @@
-import {useRouter} from 'next/router';
-import {GetSpecificRealtorsMessages} from '../../../../services/realtors.service';
-import {REALTORS_MESSAGES_PAGE_API} from '../../../../services/constants';
+import {REALTORS_MESSAGES_PAGE_API, REALTORS_SPECIFIC_MESSAGES_API} from '../../../../services/constants';
 import {MessageDetail} from '../../../../components/MessageDetail';
 
-function MessagesDetail() {
-  const router = useRouter();
-  const {messageId, realtorsId}: { messageId: string, realtorsId: string } = router.query;
-
-  const {data: messageDetail, error: messageError} = GetSpecificRealtorsMessages(realtorsId, messageId);
-
-  if (!messageDetail) return <div>Loading...</div>;
-
+function MessagesDetail({messageDetail}) {
   return <MessageDetail messageDetail={messageDetail}/>
 }
 
 export async function getServerSideProps({params, query}) {
+
   const realtor = params.realtorsId;
+  const messageId = params.messageId;
   const page = query.page || 1;
-  const res = await fetch(REALTORS_MESSAGES_PAGE_API(realtor, page));
-  const messagesData = await res.json();
+  const [messagesDataRes, messagesDetailDataRes] = await Promise.all([
+    fetch(REALTORS_MESSAGES_PAGE_API(realtor, page)),
+    fetch(REALTORS_SPECIFIC_MESSAGES_API(realtor, messageId))
+  ]);
+  const [messagesData, messageDetail] = await Promise.all([
+    messagesDataRes.json(),
+    messagesDetailDataRes.json()
+  ]);
+
   return {
-    props: {messagesData}
+    props: {messagesData, messageDetail}
   }
 }
 
